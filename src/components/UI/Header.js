96fd2ba99867be1c85f23600b6c0c2eac8e5c44c
sebamar88 <sebamar88@gmail.com>
2021-09-14 23:50:56 -0300

    Styling Header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,14 +1,20 @@
+import styles from './Header.module.css'
+
 const Header = () => {
   return ( 
-    <header>
-      <h2>Logo</h2>
-      <ul>
-        <li><a href="!#">Home</a></li>
-        <li><a href="!#">Cart</a></li>
-        <li><a href="!#">Contact</a></li>
-      </ul>
-    </header>
+    <section className={styles.section} >
+      <header className={styles.container}>
+        <a href="/">
+          <h2 className={styles.logo} >Logo</h2>
+        </a>
+        <ul className={styles.navlinks} >
+          <li><a href="!#">Home</a></li>
+          <li><a href="!#">Cart</a></li>
+          <li><a href="!#">Contact</a></li>
+        </ul>
+      </header>
+    </section>
   );
 }
- 
+
 export default Header;
